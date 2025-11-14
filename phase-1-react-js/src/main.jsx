f18bcd0b5540730bfb67c9df1eb33c261162f0b8
grantrisk <riskgrant@gmail.com>
2025-11-14 15:15:58 -0500

    feat(phase 1): update project structure and add Counter component

--- phase-1-react-js/src/main.jsx
+++ phase-1-react-js/src/main.jsx
@@ -3,7 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+// createRoot lets you create a root to display React components inside a browser DOM node.
 createRoot(document.getElementById('root')).render(
+  // <StrictMode> lets you find common bugs in your components early during development.
+  // see https://react.dev/reference/react/StrictMode#
+  // Although the Strict Mode checks only run in development, they help you find bugs that already exist in your code but can be tricky to reliably reproduce in production. Strict Mode lets you fix bugs before your users report them.
   <StrictMode>
     <App />
   </StrictMode>,
